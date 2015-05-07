77f9da6459d7a22e0ca1771bcf44a7aa670a2b7e
Scott Mann <scott.jmann@yahoo.co.uk>
2015-05-07 21:23:31 +0000

    commit test - about page

--- resources/views/templates/about.blade.php
+++ resources/views/templates/about.blade.php
@@ -2,6 +2,9 @@
     <div class="row">
         <div class="col-md-9">
             <h3>Welcome to Steel in Time</h3>
-            <p></p>
+            <p>At Steel in Time, we aim to supply affordable metals at any quantity and range of sizes. We provide various metals in large and small quantities to a range of customers including the building trade and DIY hobbyists.</p>
+            <p>We currently operate out of a 1200square foot warehouse located in Harlow, Essex but have entered the online market to expand our services to all!</p>
+            <p>We aim to give our customers the best experience possible by giving them the opportunity to buy our products without minimum order charges as well as specific cutting lengths if required. To request a cutting length please contact us <a hred="https://steelintime.co.uk/store/#/contact">here.</a></p>
+            <p>Please feel free to send us your feedback and any queries you may have and we hope you enjoy our services.</p>
         </div>
     </div>
